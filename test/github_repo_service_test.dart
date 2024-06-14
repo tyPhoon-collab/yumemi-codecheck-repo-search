3dea54fa488c1ae2f68f2d65851ad9fe42dad16e
tyPhoon <typhoonmoon1022@gmail.com>
2024-06-15 03:07:40 +0900

    Add:GitHubRepoServiceのテストの追加

---
+++ test/github_repo_service_test.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:yumemi_codecheck_repo_search/service/github_repo_service.dart';
+
+void main() {
+  test('real request', () async {
+    final service = GitHubRepoService(
+      Dio(
+        BaseOptions(
+          headers: {
+            'accept': 'application/vnd.github+json',
+          },
+        ),
+      ),
+    );
+
+    final result = await service.searchRepositories('flutter');
+
+    debugPrint(result.toString());
+
+    expect(result, isNotNull);
+    expect(result.items.length, lessThanOrEqualTo(30));
+  });
+}
