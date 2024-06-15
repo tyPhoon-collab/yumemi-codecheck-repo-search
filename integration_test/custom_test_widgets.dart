2cd375877b79c262cb79d5c36ee4a38a95b2149a
tyPhoon <typhoonmoon1022@gmail.com>
2024-06-15 19:18:17 +0900

    Add:統合テストの仮追加

---
+++ integration_test/custom_test_widgets.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:meta/meta.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:yumemi_codecheck_repo_search/main.dart';
+import 'package:yumemi_codecheck_repo_search/model/repo_search_result.dart';
+import 'package:yumemi_codecheck_repo_search/page/github_repo_search_page.dart';
+import 'package:yumemi_codecheck_repo_search/service.dart';
+import 'package:yumemi_codecheck_repo_search/service/github_repo_service.dart';
+
+import 'extension.dart';
+
+typedef WidgetTesterCallbackWithService = Future<void> Function(
+  WidgetTester tester,
+  ServiceContainer repo,
+);
+
+class MockGtHubRepoService extends Mock implements GitHubRepoService {}
+
+class ServiceContainer {
+  ServiceContainer({required this.service}) {
+    when(() => service.searchRepositories(any(), page: any(named: 'page')))
+        .thenAnswer(
+      (_) async => const RepoSearchResult(
+        totalCount: 0,
+        incompleteResults: true,
+        items: [],
+      ),
+    );
+  }
+  final GitHubRepoService service;
+}
+
+@isTest
+Future<void> testWidgetFromSearchPage(
+  String description,
+  WidgetTesterCallbackWithService callback,
+) async {
+  final service = MockGtHubRepoService();
+
+  testWidgets(description, (tester) async {
+    runApp(
+      ProviderScope(
+        overrides: [
+          gitHubRepoServiceProvider.overrideWithValue(service),
+        ],
+        child: const MyApp(),
+      ),
+    );
+
+    await tester.pumpUntilFound(find.byType(GitHubRepoSearchPage));
+
+    final container = ServiceContainer(service: service);
+
+    await callback(
+      tester,
+      container,
+    );
+  });
+}
---
+++ integration_test/extension.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+extension WidgetTesterExt on WidgetTester {
+  Future<void> tapAndSettle(Finder finder) async {
+    await tap(finder);
+    await pumpAndSettle();
+  }
+
+  Future<void> enterTextAndSettle(Finder finder, String text) async {
+    await enterText(finder, text);
+    await pumpAndSettle();
+  }
+
+  Future<void> submitAndSettle(
+    TextInputAction action,
+  ) async {
+    await testTextInput.receiveAction(action);
+    await pumpAndSettle();
+  }
+
+  /// WidgetTester.pageBackが失敗することがある。
+  /// そこで、オリジナルのページを戻るためのメソッドを追加。
+  Future<void> pageBackSafe() {
+    return tapAndSettle(find.byType(BackButtonIcon));
+  }
+
+  Future<void> pumpUntilFound(
+    Finder finder, {
+    Duration timeout = const Duration(seconds: 10),
+  }) async {
+    var wasTimeout = false;
+    final timer = Timer(timeout, () => wasTimeout = true);
+
+    while (true) {
+      await pumpAndSettle();
+      if (wasTimeout || any(finder)) break;
+    }
+    if (wasTimeout) {
+      fail('Pump until found has timed out');
+    } else {
+      timer.cancel();
+    }
+  }
+
+  Future<void> pumpUntilNotFound(
+    Finder finder, {
+    Duration timeout = const Duration(seconds: 10),
+  }) async {
+    var wasTimeout = false;
+    final timer = Timer(timeout, () => wasTimeout = true);
+
+    while (true) {
+      await pumpAndSettle();
+      if (wasTimeout || !any(finder)) break;
+    }
+    if (wasTimeout) {
+      fail('Pump until not found has timed out');
+    } else {
+      timer.cancel();
+    }
+  }
+}
