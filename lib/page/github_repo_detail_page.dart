a885dd3f1a96175e8c79ba0641dea1467fbceee7
tyPhoon <typhoonmoon1022@gmail.com>
2024-06-15 18:00:27 +0900

    Change:Ownerクラスの変更に伴う詳細ページの変更

--- lib/page/github_repo_detail_page.dart
+++ lib/page/github_repo_detail_page.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:yumemi_codecheck_repo_search/model/owner.dart';
 import 'package:yumemi_codecheck_repo_search/model/repo.dart';
 
 class GitHubRepoDetailPage extends StatelessWidget {
@@ -18,99 +19,92 @@ class GitHubRepoDetailPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: _AvatarImage(
-                    avatarUrl: repo.owner.avatarUrl,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: _AvatarImage(owner: repo.owner),
                   ),
-                ),
-                const SizedBox(height: 16),
-                InkWell(
-                  onTap: () => _launchUrl(context, repo.htmlUrl),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Flexible(
-                        child: Text(
-                          repo.fullName,
-                          style: textTheme.headlineLarge,
+                  const SizedBox(height: 16),
+                  InkWell(
+                    onTap: () => _launchUrl(context, repo.htmlUrl),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Flexible(
+                          child: Text(
+                            repo.fullName,
+                            style: textTheme.headlineLarge,
+                          ),
                         ),
+                        const SizedBox(width: 8),
+                        const Icon(Icons.launch),
+                      ],
+                    ),
+                  ),
+                  if (repo.description case final String description)
+                    Text(description, style: textTheme.bodyLarge),
+                  const SizedBox(height: 16),
+                  if (repo.language case final String language)
+                    _SVGAndText(
+                      assetName: 'assets/image/svg/file-code.svg',
+                      text: language,
+                    ),
+                  Wrap(
+                    children: [
+                      _SVGAndText(
+                        assetName: 'assets/image/svg/star.svg',
+                        text: repo.stargazersCount.toString(),
                       ),
                       const SizedBox(width: 8),
-                      const Icon(Icons.launch),
+                      _SVGAndText(
+                        assetName: 'assets/image/svg/eye.svg',
+                        text: repo.watchersCount.toString(),
+                      ),
+                      const SizedBox(width: 8),
+                      _SVGAndText(
+                        assetName: 'assets/image/svg/repo-forked.svg',
+                        text: repo.forksCount.toString(),
+                      ),
+                      const SizedBox(width: 8),
+                      _SVGAndText(
+                        assetName: 'assets/image/svg/issue-opened.svg',
+                        text: repo.openIssuesCount.toString(),
+                      ),
                     ],
                   ),
-                ),
-                if (repo.description case final String description)
-                  Text(description, style: textTheme.bodyLarge),
-                const SizedBox(height: 16),
-                if (repo.language case final String language)
-                  _SVGAndText(
-                    assetName: 'assets/image/svg/file-code.svg',
-                    text: language,
-                  ),
-                Wrap(
-                  children: [
-                    _SVGAndText(
-                      assetName: 'assets/image/svg/star.svg',
-                      text: repo.stargazersCount.toString(),
-                    ),
-                    const SizedBox(width: 8),
-                    _SVGAndText(
-                      assetName: 'assets/image/svg/eye.svg',
-                      text: repo.watchersCount.toString(),
-                    ),
-                    const SizedBox(width: 8),
-                    _SVGAndText(
-                      assetName: 'assets/image/svg/repo-forked.svg',
-                      text: repo.forksCount.toString(),
-                    ),
-                    const SizedBox(width: 8),
-                    _SVGAndText(
-                      assetName: 'assets/image/svg/issue-opened.svg',
-                      text: repo.openIssuesCount.toString(),
-                    ),
-                  ],
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
   }
-
-  Future<void> _launchUrl(BuildContext context, String url) async {
-    final uri = Uri.parse(repo.htmlUrl);
-    try {
-      await launchUrl(uri);
-    } catch (e) {
-      if (!context.mounted) return;
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to launch $uri')),
-      );
-    }
-  }
 }
 
 class _AvatarImage extends StatelessWidget {
   const _AvatarImage({
-    required this.avatarUrl,
+    required this.owner,
   });
 
-  final String avatarUrl;
+  final Owner owner;
+
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(32),
-      child: CachedNetworkImage(
-        imageUrl: avatarUrl,
-        placeholder: (context, url) => const CircularProgressIndicator(),
-        errorWidget: (context, url, error) => const Icon(Icons.error),
+    final borderRadius = BorderRadius.circular(32);
+    return InkWell(
+      onTap: () => _launchUrl(context, owner.htmlUrl),
+      borderRadius: borderRadius,
+      child: ClipRRect(
+        borderRadius: borderRadius,
+        child: CachedNetworkImage(
+          imageUrl: owner.avatarUrl,
+          placeholder: (context, url) => const CircularProgressIndicator(),
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+        ),
       ),
     );
   }
@@ -134,6 +128,19 @@ class _SVGAndText extends StatelessWidget {
         const SizedBox(width: 4),
         Text(text),
       ],
+    );
+  }
+}
+
+Future<void> _launchUrl(BuildContext context, String url) async {
+  final uri = Uri.parse(url);
+  try {
+    await launchUrl(uri);
+  } catch (e) {
+    if (!context.mounted) return;
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Failed to launch $uri')),
     );
   }
 }
