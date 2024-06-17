f6d52da032dbe7524b518dc16cbfd08025a62ac0
tyPhoon <typhoonmoon1022@gmail.com>
2024-06-18 01:07:10 +0900

    Add:ページの数字を入力できる機能の追加

--- lib/page/widget/current_page_number.dart
+++ lib/page/widget/current_page_number.dart
@@ -5,11 +5,24 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:yumemi_codecheck_repo_search/common/loading_indicator.dart';
 import 'package:yumemi_codecheck_repo_search/service.dart';
 
-class CurrentPageNumber extends ConsumerWidget {
+class CurrentPageNumber extends ConsumerStatefulWidget {
   const CurrentPageNumber({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<CurrentPageNumber> createState() => _CurrentPageNumberState();
+}
+
+class _CurrentPageNumberState extends ConsumerState<CurrentPageNumber> {
+  late final textController = TextEditingController();
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final totalCount = ref.watch(repoSearchResultProvider).value?.totalCount;
     final currentPage = ref.watch(repoSearchPageProvider);
     final perPage = ref.watch(repoSearchPerPageProvider);
@@ -26,8 +39,76 @@ class CurrentPageNumber extends ConsumerWidget {
       return (currentPage - 1) * perPage + 1;
     }
 
-    return Text(
-      '${lowerCount()} ~ ${upperCount()} of $totalCount',
+    return TextButton(
+      style: TextButton.styleFrom(
+        textStyle: const TextStyle(
+          decoration: TextDecoration.underline,
+        ),
+      ),
+      child: Text('${lowerCount()} ~ ${upperCount()} of $totalCount'),
+      onPressed: () async {
+        final page = await _showPageNumberInputDialog(context, totalCount);
+        if (page != null) {
+          ref.read(repoSearchPageProvider.notifier).update(page);
+        }
+      },
     );
+  }
+
+  Future<int?> _showPageNumberInputDialog(
+    BuildContext context,
+    int totalCount,
+  ) async {
+    final formKey = GlobalKey<FormState>();
+
+    final result = await showDialog<int>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Input Page Number'),
+          content: Form(
+            key: formKey,
+            child: TextFormField(
+              controller: textController,
+              autofocus: true,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(hintText: 'Page Number'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a number';
+                }
+                final pageNum = int.tryParse(value);
+                if (pageNum == null) {
+                  return 'Please enter a valid number';
+                }
+                if (!ref
+                    .read(repoSearchPageProvider.notifier)
+                    .validate(pageNum, totalCount)) {
+                  return 'Page number is out of range';
+                }
+                return null;
+              },
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                if (formKey.currentState!.validate()) {
+                  Navigator.of(context).pop(int.parse(textController.text));
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+    return result;
   }
 }
