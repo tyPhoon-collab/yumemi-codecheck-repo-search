2e38f77051b63469faecacff8a3ea7708d8dd6c6
tyPhoon <typhoonmoon1022@gmail.com>
2024-06-17 17:50:19 +0900

    Add:設定にテーマを変更用のUIの追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -14,21 +15,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Github Repo Search',
-      localizationsDelegates: const [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en'),
-        Locale('ja'),
-      ],
-      theme: theme,
-      darkTheme: darkTheme,
-      home: const GitHubRepoSearchPage(),
+    return AdaptiveTheme(
+      light: theme,
+      dark: darkTheme,
+      initial: AdaptiveThemeMode.system,
+      builder: (theme, darkTheme) => MaterialApp(
+        title: 'Github Repo Search',
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('en'),
+          Locale('ja'),
+        ],
+        theme: theme,
+        darkTheme: darkTheme,
+        home: const GitHubRepoSearchPage(),
+      ),
     );
   }
 }
--- lib/page/settings_page.dart
+++ lib/page/settings_page.dart
@@ -1,3 +1,4 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:app_settings/app_settings.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,7 @@ class SettingsPage extends StatelessWidget {
         child: Column(
           children: [
             _LanguageListTile(),
+            _ThemeListTile(),
           ],
         ),
       ),
@@ -31,7 +33,7 @@ class _LanguageListTile extends StatelessWidget {
       ), // 言語設定は本体設定から行うのが普通（LINEなどを参照）
       title: const Text('Language'),
       subtitle: Text(
-        _getCurrentLanguageName(
+        _getLanguageName(
           Localizations.localeOf(context),
         ),
       ),
@@ -39,11 +41,66 @@ class _LanguageListTile extends StatelessWidget {
     );
   }
 
-  String _getCurrentLanguageName(Locale locale) {
+  String _getLanguageName(Locale locale) {
     return switch (locale.languageCode) {
       'en' => 'English',
       'ja' => '日本語',
       _ => 'Unknown',
     };
+  }
+}
+
+class _ThemeListTile extends StatefulWidget {
+  const _ThemeListTile();
+
+  @override
+  State<_ThemeListTile> createState() => _ThemeListTileState();
+}
+
+class _ThemeListTileState extends State<_ThemeListTile> {
+  @override
+  Widget build(BuildContext context) {
+    final currentThemeMode = AdaptiveTheme.of(context).mode;
+
+    const modes = [
+      AdaptiveThemeMode.system,
+      AdaptiveThemeMode.light,
+      AdaptiveThemeMode.dark,
+    ];
+
+    return ListTile(
+      leading: const Icon(Icons.color_lens_outlined),
+      title: const Text('Theme'),
+      subtitle: Text(_getThemeModeName(currentThemeMode)),
+      trailing: ToggleButtons(
+        isSelected: modes.map((theme) => theme == currentThemeMode).toList(),
+        onPressed: (index) => _changeTheme(modes[index]),
+        borderRadius: const BorderRadius.all(Radius.circular(32)),
+        children: modes.map((mode) => Icon(_getThemeModeIcon(mode))).toList(),
+      ),
+    );
+  }
+
+  String _getThemeModeName(AdaptiveThemeMode mode) {
+    return switch (mode) {
+      AdaptiveThemeMode.system => 'System',
+      AdaptiveThemeMode.light => 'Light',
+      AdaptiveThemeMode.dark => 'Dark',
+    };
+  }
+
+  IconData _getThemeModeIcon(AdaptiveThemeMode mode) {
+    return switch (mode) {
+      AdaptiveThemeMode.system => Icons.brightness_4,
+      AdaptiveThemeMode.light => Icons.light_mode,
+      AdaptiveThemeMode.dark => Icons.dark_mode,
+    };
+  }
+
+  void _changeTheme(AdaptiveThemeMode newMode) {
+    // 本来ならPrefsなどを用いて永続化の処理を挟む。今回は省略
+    setState(() {
+      AdaptiveTheme.of(context).setThemeMode(newMode);
+    });
   }
 }
