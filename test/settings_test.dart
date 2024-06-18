ffa1613a7eac2636fc63d7ff0d38f091c0d3eace
tyPhoon <typhoonmoon1022@gmail.com>
2024-06-19 04:48:07 +0900

    Fix:テストが通るように修正

--- test/settings_test.dart
+++ test/settings_test.dart
@@ -1,12 +1,15 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:yumemi_codecheck_repo_search/generated/l10n.dart';
 import 'package:yumemi_codecheck_repo_search/page/settings_page.dart';
+import 'package:yumemi_codecheck_repo_search/provider/service_provider.dart';
 import 'package:yumemi_codecheck_repo_search/theme.dart';
 
 import '../integration_test/extension.dart';
+import 'mocks.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -26,6 +29,10 @@ void main() {
 
     expect(find.byKey(const Key('language_list_tile')), findsOneWidget);
     expect(find.byKey(const Key('theme_list_tile')), findsOneWidget);
+    expect(
+      find.byKey(const Key('clear_all_history_list_tile')),
+      findsOneWidget,
+    );
     expect(find.byKey(const Key('about_list_tile')), findsOneWidget);
   });
 
@@ -40,6 +47,34 @@ void main() {
 
     await tester.tapAndSettle(find.byIcon(Icons.brightness_4));
     expect(tester.currentThemeMode, AdaptiveThemeMode.system);
+  });
+
+  testWidgets('clear all history list tile is disabled when no history',
+      (WidgetTester tester) async {
+    await _buildWidget(tester);
+    _expectEnable(find.byKey(const Key('clear_all_history_list_tile')), false);
+  });
+
+  testWidgets('clear all history', (WidgetTester tester) async {
+    await _buildWidget(tester, history: ['1', '2', '3']);
+
+    await tester.pumpAndSettle();
+
+    _expectEnable(find.byKey(const Key('clear_all_history_list_tile')), true);
+    await tester
+        .tapAndSettle(find.byKey(const Key('clear_all_history_list_tile')));
+
+    expect(find.byType(Dialog), findsOneWidget);
+
+    await tester.tapAndSettle(find.text(S.current.clear));
+
+    expect(find.text(S.current.clearHistorySuccess), findsOneWidget);
+    expect(
+      await ProviderScope.containerOf(
+        tester.element(find.byType(MaterialApp)),
+      ).read(queryHistoryServiceProvider).getAll(),
+      isEmpty,
+    );
   });
 
   testWidgets('shows about dialog with correct version',
@@ -58,18 +93,28 @@ void main() {
 }
 
 Future<void> _buildWidget(
-  WidgetTester tester,
-) async {
+  WidgetTester tester, {
+  List<String>? history,
+}) async {
   await tester.pumpWidget(
-    AdaptiveTheme(
-      initial: AdaptiveThemeMode.system,
-      light: theme,
-      dark: darkTheme,
-      builder: (theme, darkTheme) => MaterialApp(
-        localizationsDelegates: const [S.delegate],
-        theme: theme,
-        darkTheme: darkTheme,
-        home: const Scaffold(body: SettingsPage()),
+    ProviderScope(
+      overrides: [
+        queryHistoryServiceImplProvider.overrideWith((_) {
+          final impl = MockQueryHistoryService();
+          registerMockQueryHistoryServiceWhens(impl, history ?? []);
+          return impl;
+        }),
+      ],
+      child: AdaptiveTheme(
+        initial: AdaptiveThemeMode.system,
+        light: theme,
+        dark: darkTheme,
+        builder: (theme, darkTheme) => MaterialApp(
+          localizationsDelegates: const [S.delegate],
+          theme: theme,
+          darkTheme: darkTheme,
+          home: const SettingsPage(),
+        ),
       ),
     ),
   );
@@ -78,4 +123,11 @@ Future<void> _buildWidget(
 extension _GetCurrentThemeMode on WidgetTester {
   AdaptiveThemeMode get currentThemeMode =>
       AdaptiveTheme.of(element(find.byType(MaterialApp))).mode;
+}
+
+void _expectEnable(Finder finder, bool enabled) {
+  expect(
+    (finder.evaluate().first.widget as ListTile).enabled,
+    enabled,
+  );
 }
