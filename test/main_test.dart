7455f892c1d76723dce32ea79167e2fff11cd02d
tyPhoon <typhoonmoon1022@gmail.com>
2024-06-21 15:45:28 +0900

    Add:main関数呼び出しのテストの追加

---
+++ test/main_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:yumemi_codecheck_repo_search/main.dart' as app;
+
+void main() {
+  testWidgets('run main', (WidgetTester tester) async {
+    app.main();
+  });
+}
